532da50acbd5d72ea2d5e29f02f5a1212e11ae50
shrihari1515 <shriharis1515@gmail.com>
2022-03-07 13:45:31 +0530

    commit

---
+++ sqlwebapp/WebApplication1/WebForm1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <asp:Label ID="Label1" runat="server" Text="type ur name"></asp:Label>
+            <asp:Button ID="Button2" runat="server" Text="submit" OnClick="Button2_Click" />
+        </div>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ sqlwebapp/WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack == true)
+            {
+                Label1.Text = ("great");
+            }
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection storm = new SqlConnection("Server=tcp:hari12345.database.windows.net,1433;Initial Catalog=hari123456;Persist Security Info=False;User ID=hari;Password=Giri@20052005;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            {
+                SqlCommand insert = new SqlCommand("EXEC dbo.InsertFullname @Fullname", storm);
+                insert.Parameters.AddWithValue("@Fullname", TextBox1.Text);
+                storm.Open();
+                insert.ExecuteNonQuery();
+                storm.Close();
+                if (IsPostBack)
+                {
+                    TextBox1.Text = "";
+                }
+            }
+
+
+        }
+    }
+}
